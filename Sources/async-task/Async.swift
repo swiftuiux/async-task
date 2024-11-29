ab8b92e7c4a973465cfcd77fc720d3152486e0ae
Igor <igorshelopaev@gmail.com>
2024-11-29 13:12:24 +0100

    Update Async.swift

--- Sources/async-task/Async.swift
+++ Sources/async-task/Async.swift
@@ -76,9 +76,14 @@ public extension Async {
         }
     }
     
+    /// An enumeration representing the properties of an asynchronous task that can be reset.
     @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
-    enum TaskProperty{
+    enum TaskProperty {
+
+        /// Represents the `error` property of a task.
         case error
+
+        /// Represents the `value` property of a task.
         case value
     }
 }
